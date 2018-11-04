ae7b86c51352b4716ca1df44323c4905c3c6827c
keep-it-low <30688221+keep-it-low@users.noreply.github.com>
2018-11-04 11:53:32 +0300

    Update 01-11#3.js

---
+++ 01-11#3.js
@@ -0,0 +1,7 @@
+function parametr (a){
+  if (a){
+  console.log('Введенное значение не является null или undefined')}
+else {
+  console.log('Введенное значение является null или undefined')
+  }
+  }
